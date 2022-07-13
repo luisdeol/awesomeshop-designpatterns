b68a760e75289f52e8e871223a5d2de4855b1e75
Luis Felipe <luisfelipekai@gmail.com>
2022-07-13 10:12:26 -0300

    Decorator OK

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using AwesomeShopDesignPatterns.API.Application;
 using AwesomeShopDesignPatterns.API.Application.NotifyUser;
 using AwesomeShopDesignPatterns.API.Infrastructure.Services;
 using SendGrid.Extensions.DependencyInjection;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,8 @@ builder.Services.AddSendGrid(o => o.ApiKey = sendGridApiKey);
 
 builder.Services.AddScoped<INotificationFactoryService, NotificationFactoryService>();
 
-builder.Services.AddScoped<ICommandHandler<NotifyUserCommand, Task>, NotifyUserCommandHandler>();
+builder.Services.AddScoped<NotifyUserCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<NotifyUserCommand, Task>, NotifyUserCommandHandlerDecorator>();
 
 builder.Services.AddScoped<IMediator, Mediator>();
 
@@ -20,6 +22,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) => {
+    Serilog.Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+}).UseSerilog();
+
 
 var app = builder.Build();
 
